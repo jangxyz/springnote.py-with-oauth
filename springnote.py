75e25a416a85ce4bffa344994fa0ddcd9ee0f3bf
Kim, Jang-hwan <janghwan@gmail.com>
2010-03-10 01:02:29 +0900

    update current to v0.2

--- springnote.py
+++ springnote.py
@@ -6,14 +6,15 @@
     Springnote API library using OAuth
 
 """
-VERSION = 0.1
+__author__  = "Jang-hwan Kim"
+__email__   = "janghwan at gmail dot com"
+__version__ = 0.2
 
 import env 
 
-import oauth, sys, types
+import oauth, sys
 import simplejson as json
-import httplib, urllib
-import socket
+import httplib, urllib, socket
 
 # default consumer token (as springnote python library)
 CONSUMER_TOKEN_KEY    = '162DSyqm28o355V7zEKw'
@@ -21,6 +22,42 @@ CONSUMER_TOKEN_SECRET = 'MtiDOAWsFkH2yOLzOYkubwFK6THOA5iAJIR4MJnwKMQ'
 
 default_verbose = False
 default_dry_run = False
+
+
+class SpringnoteError:
+    class Base(Exception):
+        def __init__(self, error):
+            self.error = error
+        def __str__(self):
+            error_tuple = lambda x: (x["error"]["error_type"], x["error"]["description"])
+            error_tuple_str = lambda x: "%s: %s" % error_tuple(x)
+            if isinstance(self.error, dict):
+                return error_tuple_str(self.error)
+            elif isinstance(self.error, list):
+                error_msgs = [error_tuple_str(error) for error in self.error]
+                return '\n'.join(error_msgs)
+            else:
+                return self.error
+    class ParseError(Base): pass
+    class Unauthorized(Base): pass
+    class NotFound(Base):     pass
+    class Network(Base):      pass
+
+    class Response(Base):
+        # status codes and names, extracted from httplib
+        status_map = {100: 'CONTINUE', 101: 'SWITCHING_PROTOCOLS', 102: 'PROCESSING', 200: 'OK', 201: 'CREATED', 202: 'ACCEPTED', 203: 'NON_AUTHORITATIVE_INFORMATION', 204: 'NO_CONTENT', 205: 'RESET_CONTENT', 206: 'PARTIAL_CONTENT', 207: 'MULTI_STATUS', 226: 'IM_USED', 300: 'MULTIPLE_CHOICES', 301: 'MOVED_PERMANENTLY', 302: 'FOUND', 303: 'SEE_OTHER', 304: 'NOT_MODIFIED', 305: 'USE_PROXY', 307: 'TEMPORARY_REDIRECT', 400: 'BAD_REQUEST', 401: 'UNAUTHORIZED', 402: 'PAYMENT_REQUIRED', 403: 'FORBIDDEN', 404: 'NOT_FOUND', 405: 'METHOD_NOT_ALLOWED', 406: 'NOT_ACCEPTABLE', 407: 'PROXY_AUTHENTICATION_REQUIRED', 408: 'REQUEST_TIMEOUT', 409: 'CONFLICT', 410: 'GONE', 411: 'LENGTH_REQUIRED', 412: 'PRECONDITION_FAILED', 413: 'REQUEST_ENTITY_TOO_LARGE', 414: 'REQUEST_URI_TOO_LONG', 415: 'UNSUPPORTED_MEDIA_TYPE', 416: 'REQUESTED_RANGE_NOT_SATISFIABLE', 417: 'EXPECTATION_FAILED', 422: 'UNPROCESSABLE_ENTITY', 423: 'LOCKED', 424: 'FAILED_DEPENDENCY', 426: 'UPGRADE_REQUIRED', 443: 'HTTPS_PORT', 500: 'INTERNAL_SERVER_ERROR', 501: 'NOT_IMPLEMENTED', 502: 'BAD_GATEWAY', 503: 'SERVICE_UNAVAILABLE', 504: 'GATEWAY_TIMEOUT', 505: 'HTTP_VERSION_NOT_SUPPORTED', 507: 'INSUFFICIENT_STORAGE', 510: 'NOT_EXTENDED'}
+
+        def __init__(self, response):
+            body = response.read()
+            try:
+                errors = json.loads(body)
+            except ValueError:
+                errors = body
+
+            status_name = self.status_map[response.status].replace('_', ' ')
+            self.status = response.status
+            self.error  = "%d %s: %s" % (response.status, status_name, errors)
+
 
 def is_verbose(is_verbose):
     if (is_verbose is None and default_verbose is True) or is_verbose is True:
@@ -296,40 +333,6 @@ class Springnote:
     def get_comments(self, id, note=None, params={}, verbose=None):
         raise NotImplementedError('you should implement it!')
 
-
-class SpringnoteError:
-    class Base(Exception):
-        def __init__(self, error):
-            self.error = error
-        def __str__(self):
-            error_tuple = lambda x: (x["error"]["error_type"], x["error"]["description"])
-            error_tuple_str = lambda x: "%s: %s" % error_tuple(x)
-            if isinstance(self.error, dict):
-                return error_tuple_str(self.error)
-            elif isinstance(self.error, list):
-                error_msgs = [error_tuple_str(error) for error in self.error]
-                return '\n'.join(error_msgs)
-            else:
-                return self.error
-    class ParseError(Base): pass
-    class Unauthorized(Base): pass
-    class NotFound(Base):     pass
-    class Network(Base):      pass
-
-    class Response(Base):
-        # status codes and names, extracted from httplib
-        status_map = {100: 'CONTINUE', 101: 'SWITCHING_PROTOCOLS', 102: 'PROCESSING', 200: 'OK', 201: 'CREATED', 202: 'ACCEPTED', 203: 'NON_AUTHORITATIVE_INFORMATION', 204: 'NO_CONTENT', 205: 'RESET_CONTENT', 206: 'PARTIAL_CONTENT', 207: 'MULTI_STATUS', 226: 'IM_USED', 300: 'MULTIPLE_CHOICES', 301: 'MOVED_PERMANENTLY', 302: 'FOUND', 303: 'SEE_OTHER', 304: 'NOT_MODIFIED', 305: 'USE_PROXY', 307: 'TEMPORARY_REDIRECT', 400: 'BAD_REQUEST', 401: 'UNAUTHORIZED', 402: 'PAYMENT_REQUIRED', 403: 'FORBIDDEN', 404: 'NOT_FOUND', 405: 'METHOD_NOT_ALLOWED', 406: 'NOT_ACCEPTABLE', 407: 'PROXY_AUTHENTICATION_REQUIRED', 408: 'REQUEST_TIMEOUT', 409: 'CONFLICT', 410: 'GONE', 411: 'LENGTH_REQUIRED', 412: 'PRECONDITION_FAILED', 413: 'REQUEST_ENTITY_TOO_LARGE', 414: 'REQUEST_URI_TOO_LONG', 415: 'UNSUPPORTED_MEDIA_TYPE', 416: 'REQUESTED_RANGE_NOT_SATISFIABLE', 417: 'EXPECTATION_FAILED', 422: 'UNPROCESSABLE_ENTITY', 423: 'LOCKED', 424: 'FAILED_DEPENDENCY', 426: 'UPGRADE_REQUIRED', 443: 'HTTPS_PORT', 500: 'INTERNAL_SERVER_ERROR', 501: 'NOT_IMPLEMENTED', 502: 'BAD_GATEWAY', 503: 'SERVICE_UNAVAILABLE', 504: 'GATEWAY_TIMEOUT', 505: 'HTTP_VERSION_NOT_SUPPORTED', 507: 'INSUFFICIENT_STORAGE', 510: 'NOT_EXTENDED'}
-
-        def __init__(self, response):
-            body = response.read()
-            try:
-                errors = json.loads(body)
-            except ValueError:
-                errors = body
-
-            status_name = self.status_map[response.status].replace('_', ' ')
-            self.status = response.status
-            self.error  = "%d %s: %s" % (response.status, status_name, errors)
 
 
 
