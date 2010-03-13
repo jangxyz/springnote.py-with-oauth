bec40a9508643480444d8a0c719ce3a94c3a33db
Kim, Jang-hwan <janghwan@gmail.com>
2010-03-13 17:37:11 +0900

    change StringContainsConstraint(=string_contains) to accept regular expression
     * not that I'm using it, right now..
     * merge with DictionaryNotContainsValueConstraint

--- test/lib/pmock_xtnd.py
+++ test/lib/pmock_xtnd.py
@@ -22,6 +22,7 @@ class DictionarySubsetConstraint(object):
 def dict_including(expected):
     return DictionarySubsetConstraint(expected)
 
+
 ##
 ##
 
@@ -36,4 +37,27 @@ class DictionaryNotContainsValueConstraint(object):
 def not_contains_value(expected):
     return DictionaryNotContainsValueConstraint(expected)
 
+##
+##
 
+
+class StringContainsConstraint(object):
+    ''' patch of StringcontainsConstraint to accept regex '''
+    def __init__(self, expected):
+        self._expected = expected
+    def __repr__(self):
+        return "%s.string_contains(%s)" % (__name__, repr(self._expected))
+    def eval(self, arg):
+        import types, re
+        if arg is None:
+            return False
+        elif isinstance(self._expected, types.StringType):
+            return arg.find(self._expected) != -1
+        elif isinstance(self._expected, re._pattern_type):
+            return self._expected.search(arg) is not None
+        else:
+            return False
+
+
+def string_contains(expected):
+    return StringContainsConstraint(expected)
